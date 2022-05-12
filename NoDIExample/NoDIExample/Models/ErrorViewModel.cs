3e58810a27379c8eb075a2a43cec7e9a41eb965b
SilentHill <nikich213@gmail.com>
2022-05-12 22:45:04 +0500

    Add new example without DI.

---
+++ NoDIExample/NoDIExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoDIExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
