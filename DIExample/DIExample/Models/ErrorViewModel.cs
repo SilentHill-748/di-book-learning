8c0754f5aa4b7d9e4589c66944efe3d1f05cde6f
SilentHill <nikich213@gmail.com>
2022-05-13 17:13:59 +0500

    Added a new project with DI.

---
+++ DIExample/DIExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DIExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
