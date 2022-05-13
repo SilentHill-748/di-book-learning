bc3baaf63ba7664c47b068eb4dee960e39758eb8
SilentHill <nikich213@gmail.com>
2022-05-13 18:49:30 +0500

    Added discount calculating method.

--- DIExample/DIExample.Domain/Entities/Product.cs
+++ DIExample/DIExample.Domain/Entities/Product.cs
@@ -1,11 +1,21 @@
-﻿namespace DIExample.Domain.Entities
+﻿using DIExample.Domain.POCOs;
+
+namespace DIExample.Domain.Entities
 {
     public class Product
     {
-        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
         public decimal UnitPrice { get; set; }
         public bool IsFeatured { get; set; }
+
+
+        public DiscountedProduct GetFeaturedProduct(IUserContext userContext)
+        {
+            bool isPrefferedCustomer = userContext.IsInRole(Role.PreferredCustomer);
+
+            decimal discount = isPrefferedCustomer ? 0.95m : 1;
+
+            return new DiscountedProduct(Name, UnitPrice * discount);
+        }
     }
 }
