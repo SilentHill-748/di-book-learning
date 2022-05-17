7fc6b48062721a2fa5d5dcf888ec3f8a59691936
SilentHill <nikich213@gmail.com>
2022-05-17 23:47:10 +0500

    Added implementation of IUserContext.

---
+++ DIExample/DIExample/AspNetUserContextAdapter.cs
@@ -0,0 +1,18 @@
+﻿using DIExample.Domain;
+
+namespace DIExample
+{
+    public class AspNetUserContextAdapter : IUserContext
+    {
+        private readonly static HttpContextAccessor Accessor = new();
+
+
+        public bool IsInRole(Role role)
+        {
+            if (Accessor.HttpContext is null)
+                return false;
+
+            return Accessor.HttpContext.User.IsInRole(role.ToString());
+        }
+    }
+}
