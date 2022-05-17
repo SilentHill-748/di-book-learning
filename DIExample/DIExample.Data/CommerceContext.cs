a9c3b5b7b7a2f2c61638484acd10d54c75158669
SilentHill <nikich213@gmail.com>
2022-05-17 23:41:17 +0500

    Added db context and implementation of IProductRepository.

---
+++ DIExample/DIExample.Data/CommerceContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using DIExample.Domain.Entities;
+
+namespace DIExample.Data
+{
+    public class CommerceContext : DbContext
+    {
+        private readonly string _connectionString;
+
+
+        public CommerceContext(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentException($"Parameter \'{nameof(connectionString)}\' was be null or empty!");
+
+            this._connectionString = connectionString;
+        }
+
+
+        public DbSet<Product> Products => Set<Product>();
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ DIExample/DIExample.Data/Repositories/SqlProductsRepository.cs
@@ -0,0 +1,33 @@
+﻿// TODO: Абстракции домена лучше выделить в отдельную сборку Domain.Abstractions и уже эту сборку юзать.
+// В книге 'Внедрение зависимостей на платформе .NET' авторы упоминули, что абстракцию IProductService надо
+// реализовать в отдельной сборке. Я предполагаю, что все абстракции домена надо вынести в отдельную сборку
+// Domain.Abstractions.
+using DIExample.Domain.Repositories;
+using DIExample.Domain.Entities;
+
+namespace DIExample.Data.Repositories
+{
+    public class SqlProductsRepository : IProductRepository
+    {
+        private readonly CommerceContext _dbContext;
+
+
+        // Атворы книг пишут, что нужно использовать абстракцию к нестабильным зависимостям,
+        // но в примере с использованием технологий DI они явно указали контекст данных.
+        // По идее, нам может потребоваться в будущем другой контекст.
+        // Лучше же использовать дженерик версию репозиториев? Для следования примеру я опустил этот вопрос.
+        public SqlProductsRepository(CommerceContext dbContext)
+        {
+            ArgumentNullException.ThrowIfNull(dbContext, nameof(dbContext));
+
+            _dbContext = dbContext;
+        }
+
+
+        public IEnumerable<Product> GetFeaturedProducts()
+        {
+            return _dbContext.Products
+                .Where(product => product.IsFeatured);
+        }
+    }
+}
